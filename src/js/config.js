05f22fa00e83b17e2ad3133e88e68c482ffe5747
Sahid Midda <midda.sahid@gmail.com>
2023-11-02 21:33:04 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b215afd4-1e97-400b-8edf-1b110a6872bb';
+export const MODAL_CLOSE_SEC = 2.5;
